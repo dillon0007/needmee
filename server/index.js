23d5c6e846357fffc7f129498813552171f49007
dillon0007 <dillongirkar30@gmail.com>
2022-08-30 21:32:19 +0530

    install backend packages like express, node, mongoose, http

---
+++ server/index.js
@@ -0,0 +1,17 @@
+console.log("Heloo World");
+
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+// Creating an API
+//GET, PUT, POST, Delete, UPDATE -> CRUD
+
+
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`connected at port ${PORT} hello`);
+
+});
